d9b23fe3f39396bae5032475e4a9f84fc00098d8
Thomas Schneider <tschneit717@gmail.com>
2021-06-18 12:18:00 -0400

    Initial commit and POC

---
+++ src/components/text/Text.tsx
@@ -0,0 +1,21 @@
+import { PropsWithChildren, FunctionComponent } from "react";
+
+export interface TextProps extends PropsWithChildren<any> {
+  text: string;
+  textTag: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p";
+}
+
+export const TextEditConfig = {
+  emptyLabel: "Text Component",
+  isEmpty: function (props: TextProps): boolean {
+    return !props || !props.text || !props.type;
+  },
+};
+
+export const Text: FunctionComponent<TextProps> = (props: TextProps) => {
+  const TextWrapper = (textProps: TextProps): JSX.Element => {
+    const { textTag: TextType = "p", text: Text } = textProps;
+    return <TextType>{Text}</TextType>;
+  };
+  return <TextWrapper textTag={props.type} text={props.text}></TextWrapper>;
+};
