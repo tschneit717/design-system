194c3304acdbb7182d6fff109b8e0292015df221
tschneit717 <tschneit717@gmail.com>
2021-06-28 21:24:02 -0400

    Created accordion component

--- src/components/accordion/Accordion.test.tsx
+++ src/components/accordion/Accordion.test.tsx
@@ -1,57 +1,57 @@
-import { Accordion } from "./Accordion";
-import React from "react";
-import Enzyme, { shallow } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-import { cleanup, render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { Accordion } from './Accordion';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { cleanup, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
-describe("Accordion ->", () => {
+describe('Accordion ->', () => {
   Enzyme.configure({ adapter: new Adapter() });
 
   const ACCORDION_ITEMS = [
     {
-      title: "Title 1",
-      body: "Body 1",
+      title: 'Title 1',
+      body: 'Body 1',
     },
     {
-      title: "Title 2",
-      body: "Body 2",
+      title: 'Title 2',
+      body: 'Body 2',
     },
     {
-      title: "Title 3",
-      body: "Body 3",
+      title: 'Title 3',
+      body: 'Body 3',
     },
     {
-      title: "Title 4",
-      body: "Body 4",
+      title: 'Title 4',
+      body: 'Body 4',
     },
   ];
 
-  const TEST_ID = "accordion-test-id";
-
-  beforeEach(() => {});
+  const TEST_ID = 'accordion-test-id';
 
   afterEach(() => {
     cleanup();
   });
 
-  test("renders when title prop is provided", async () => {
+  test('renders when title prop is provided', async () => {
     render(<Accordion testid={TEST_ID} accordionItems={ACCORDION_ITEMS} />);
+    expect(screen.getByTestId(TEST_ID)).toBeTruthy();
   });
 
-  test("renders the number of accordion items", async () => {
+  test('renders the number of accordion items', async () => {
     render(<Accordion testid={TEST_ID} accordionItems={ACCORDION_ITEMS} />);
 
     expect(screen.getByTestId(TEST_ID).children.length).toEqual(4);
   });
 
-  test("toggles the accordion item to open on click", async () => {
+  test('toggles the accordion item to open on click and close on click', async () => {
     render(<Accordion testid={TEST_ID} accordionItems={ACCORDION_ITEMS} />);
 
     const leftClick = { button: 0 };
-    userEvent.click(screen.getByText(ACCORDION_ITEMS[1].title), leftClick);
-    expect(screen.getByTitle(ACCORDION_ITEMS[1].title).classList).toHaveClass(
-      "open"
-    );
+    const accordionItem = screen.getByTitle(ACCORDION_ITEMS[1].title);
+
+    userEvent.click(accordionItem, leftClick);
+    expect(accordionItem).toHaveClass('open');
+    userEvent.click(accordionItem, leftClick);
+    expect(accordionItem).not.toHaveClass('open');
   });
 });
--- src/components/accordion/Accordion.tsx
+++ src/components/accordion/Accordion.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { PropsWithChildren, FunctionComponent } from "react";
+import { useCallback, useState } from 'react';
+import { PropsWithChildren, FunctionComponent } from 'react';
 
 export interface AccordionProps extends PropsWithChildren<any> {
   accordionItems: Array<{
@@ -10,26 +10,33 @@ export interface AccordionProps extends PropsWithChildren<any> {
 }
 
 export const AccordionEditConfig = {
-  emptyLabel: "Accordion Component",
+  emptyLabel: 'Accordion Component',
   isEmpty: function (props: AccordionProps): boolean {
     return !props || !props.accordionItems.length;
   },
 };
 
+const useToggle = (initialValue = false): (boolean | (() => void))[] => {
+  const [value, setValue] = useState(initialValue);
+  const toggle = useCallback(() => {
+    setValue((v) => !v);
+  }, []);
+  return [value, toggle];
+};
+
 export const Accordion: FunctionComponent<AccordionProps> = (
   props: AccordionProps
 ) => {
-  const [activeTab, setActiveTab] = useState();
+  const [isOn, toggleIsOn] = useToggle();
   return (
     <div data-testid={props.testid}>
       {props.accordionItems.map((item) => (
         <button
           key={item.title}
-          onClick={setActiveTab}
+          onClick={toggleIsOn as any}
           title={item.title}
-          className="accordion-body"
-        >
-          <span className="accordion-title">{item.title}</span>
+          className={`accordion-body ${isOn ? 'open' : ''}`}>
+          <span className='accordion-title'>{item.title}</span>
           <div>{item.body}</div>
         </button>
       ))}
