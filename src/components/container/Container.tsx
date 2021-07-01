cd98eeaa18f45128efe96414ce6df7688ac5b4ea
tschneit717 <tschneit717@gmail.com>
2021-07-01 18:05:36 -0400

    Created container component

--- src/components/container/Container.tsx
+++ src/components/container/Container.tsx
@@ -1,15 +1,16 @@
 import { PropsWithChildren, FunctionComponent } from 'react';
-
+import { v4 as uuidv4 } from 'uuid';
 export interface ContainerProps extends PropsWithChildren<any> {
-  text: string;
+  children: JSX.Element[] | JSX.Element;
+  testid?: string;
 }
 
 export const Container: FunctionComponent<ContainerProps> = (
   props: ContainerProps
 ) => {
   return (
-    <div>
-      <p>Container</p>
+    <div key={uuidv4()} data-testid={props.testid} className='container'>
+      {props.children}
     </div>
   );
 };
